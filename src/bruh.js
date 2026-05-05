54df8d132e98b453a4eb79a90777dba665669d09
anthonypark0 <park.ant@northeastern.edu>
2026-05-05 18:43:30 -0400

    new

--- src/bruh.js
+++ src/bruh.js
@@ -3,137 +3,126 @@ import { useEffect, useState, useRef } from "react";
 import Proph from './proph';
 
 function Bruh() {
-    const [input, setInput] = useState("");
-    const [outputHistory, setOutputHistory] = useState([]);
-    const inputRef = useRef();
-    const [startScreen, setstartScreen] = useState(false);
+  const [input, setInput] = useState("");
+  const [outputHistory, setOutputHistory] = useState([]);
+  const [startScreen, setstartScreen] = useState(false);
+  const [gamestart, setGamestart] = useState(false);
+  const inputRef = useRef();
 
-    useEffect(() => {
-        if (!gamestart) {
-            inputRef.current.focus();
-        }
+  useEffect(() => {
+    if (!gamestart && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [gamestart]);
 
-        
-    }, []);
+  useEffect(() => {
+    setstartScreen(outputHistory.length === 0 && !gamestart);
+  }, [outputHistory, gamestart]);
 
-    useEffect(() => {
-        // Set gamestart to true if outputHistory is empty
-        setstartScreen(outputHistory.length === 0 && !gamestart);
-    }, [outputHistory]);
+  const maxHistorySize = 7;
 
-    const maxHistorySize = 7; // Maximum number of lines to keep in history
-    const [gamestart, setGamestart] = useState(false);
-    const handleCommand = (command) => {
-        let newOutput = [...outputHistory];
+  const handleCommand = (command) => {
+    let newOutput = [...outputHistory];
 
-        switch (command.trim()) {
-            case "clear":
-                newOutput = [];
-                break;
-            case "hello":
-                newOutput.push("$ " + command + "\nHello World!");
-                break;
-            case "waste time":
-                newOutput.push("$ " + command + "\nUse the commands \"start game\" and \"stop game\" to play an unfinished (but usable) version of \nBlackJack");
-                break;
-            case "about":
-                newOutput = [];
-                newOutput.push((<img src="IMG_4347.JPG" alt="Logo of the website" style={{ width: '300px', height: '200px' }}/>))
-                newOutput.push("Hi! I'm Anthony Park. I'm a recent graduate of Northeastern University (Class of 2026)." +
-                    " I majored in Computer Science, with a concentration in software. I am currently seeking a full-time position. If " +
-                    "you're looking for more information on"
-                    + " my skills, projects, and work experience, type \"resume\" or \"github\". For contact information, type \"contact\". For other commands, type \"help\"")
-                break;
-            case "help":
-                newOutput.push("$ " + command + "\nAvailable Commands: about, resume, github, contact, hello, waste time, clear");
-                break;
-            case "resume":
-                newOutput.push("$ " + command + "\nDownload Link:");
-                newOutput.push((<a href="Anthony_Park_Resume.pdf" download>Anthony Park Resume 2026</a>));
-                break;
-            case "contact":
-                newOutput.push("$ " + command + "\nEmail: park.ant@northeastern.edu" + "\nLinkedIn: ");
-                newOutput.push((<a href='https://www.linkedin.com/in/anthony-park0'>https://www.linkedin.com/in/anthony-park0</a>));
-                break;
-            case "start game":
-                newOutput = [];
-                setGamestart(true);
-                break;
-            case "stop game":
-                setGamestart(false);
-                break;
-            case "ls":
-                newOutput.push("$ " + command + "\nList of projects");
-                break;
-            case "pwd":
-                newOutput.push("$ " + command + "\nYou're on my terminal site");
-                break;
-            case "github":
-                newOutput.push("$ " + command + "\nVisit my GitHub profile: ");
-                newOutput.push((<a href='https://github.com/anthonypark0'>https://github.com/anthonypark0</a>));
-                break;
-            default:
-                newOutput.push("$ " + command + "\nUnknown Command, try help for a list of commands");
-        }
+    switch (command.trim()) {
+      case "clear":
+        newOutput = [];
+        break;
+      case "hello":
+        newOutput.push("$ " + command + "\nHello World!");
+        break;
+      case "waste time":
+        newOutput.push("$ " + command + "\nUse the commands \"start game\" and \"stop game\" to play an unfinished (but usable) version of \nBlackJack");
+        break;
+      case "about":
+        newOutput = [];
+        newOutput.push(
+          <img src="IMG_4347.JPG" alt="Anthony Park" style={{ width: '300px', height: '200px' }} />
+        );
+        newOutput.push(
+          "Hi! I'm Anthony Park. I'm a recent graduate of Northeastern University (Class of 2026). " +
+          "I majored in Computer Science, with a concentration in software. I am currently seeking a full-time position. If " +
+          "you're looking for more information on my skills, projects, and work experience, type \"resume\" or \"github\". " +
+          "For contact information, type \"contact\". For other commands, type \"help\""
+        );
+        break;
+      case "help":
+        newOutput.push("$ " + command + "\nAvailable Commands: about, resume, github, contact, hello, waste time, clear");
+        break;
+      case "resume":
+        newOutput.push("$ " + command + "\nDownload Link:");
+        newOutput.push(<a href="Anthony_Park_Resume.pdf" download>Anthony Park Resume 2026</a>);
+        break;
+      case "contact":
+        newOutput.push("$ " + command + "\nEmail: park.ant@northeastern.edu\nLinkedIn: ");
+        newOutput.push(<a href='https://www.linkedin.com/in/anthony-park0'>https://www.linkedin.com/in/anthony-park0</a>);
+        break;
+      case "start game":
+        newOutput = [];
+        setGamestart(true);
+        break;
+      case "stop game":
+        setGamestart(false);
+        break;
+      case "ls":
+        newOutput.push("$ " + command + "\nList of projects");
+        break;
+      case "pwd":
+        newOutput.push("$ " + command + "\nYou're on my terminal site");
+        break;
+      case "github":
+        newOutput.push("$ " + command + "\nVisit my GitHub profile: ");
+        newOutput.push(<a href='https://github.com/anthonypark0'>https://github.com/anthonypark0</a>);
+        break;
+      default:
+        newOutput.push("$ " + command + "\nUnknown Command, try help for a list of commands");
+    }
 
-        // Trim the history to maxHistorySize
-        if (newOutput.length > maxHistorySize) {
-            newOutput = newOutput.slice(-maxHistorySize);
-        }
+    if (newOutput.length > maxHistorySize) {
+      newOutput = newOutput.slice(-maxHistorySize);
+    }
 
-        setOutputHistory(newOutput);
-        setInput("");
-    };
+    setOutputHistory(newOutput);
+    setInput("");
+  };
 
-    return (
-        <html>
-            <head>
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet" />
-            </head>
-            
+  return (
+    <div id="screen" onClick={() => inputRef.current.focus()}>
+      <img src="scanlines.png" id="scan" className="noselect" alt="" />
+      <img src="bezel.png" id="bezel" className="noselect" alt="" />
 
-            <body>
-                
-                <div id="screen" onClick={e => { inputRef.current.focus() }}>
-                    <img src="scanlines.png" id="scan" className="noselect" />
-                   <img src="bezel.png" id="bezel" className="noselect" /> 
+      <div id="content">
+        <input
+          ref={inputRef}
+          id="mine"
+          type="text"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          onKeyDown={e => {
+            if (e.key === "Enter") {
+              handleCommand(input);
+            }
+          }}
+        />
 
-                    <div id="content">
-                       
-                        < input
-                            ref={inputRef}
-                            id="mine"
-                            type="text"
-                            value={input}
-                            onChange={e => setInput(e.target.value)}
-                            onKeyDown={e => {
-                                if (e.key === "Enter") {
-                                    handleCommand(input);
-                                }
+        <div id="terminal">
+          <div id="startText" className="animated-text">
+            {startScreen && (
+              <div>Welcome to my Website! My name is Anthony Park. Type "help" for list of commands.</div>
+            )}
+          </div>
 
-                            }}
-                        />
+          {gamestart && <Proph />}
 
-                        <div id="terminal">
-                            <div id="startText" className="animated-text" >
-                                {startScreen && <div>  Welcome to my Website! My name is Anthony Park. Type "help" for list of commands.</div>}
-                            </div>
-                            {gamestart && <Proph />}
-                            <div >
-                            {outputHistory.map((line, index) => (
-                                <div key={index}>{line}</div>
-
-                            ))}
-                            </div>
-                        
-                        </div>
-                    </div>
-                </div>
-
-            </body>
-        </html>
-    );
+          <div>
+            {outputHistory.map((line, index) => (
+              <div key={index}>{line}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Bruh;
